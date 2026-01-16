c62dd4fc698f7df1f554cb3b61da0c57d348d9fd
Adithya-b-03 <adithyaakash0304@gmail.com>
2026-01-16 21:26:09 +0530

    Add gunicorn for Render start command

--- app.py
+++ app.py
@@ -104,5 +104,6 @@ def chat():
     response = rag_chain.invoke({"input": msg})
     return str(response["answer"])
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
